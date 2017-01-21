a2715c13601745ced9bdbd247f3a4eccdb59603b
Tenzin Sonam <tenso2006@gmail.com>
2017-01-21 11:37:54 -0800

    completed routes

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,18 +1,17 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-    $scope.data = {};
+  $scope.data = {};
 
-    var initialize = function () {
-      Links.getAll()
-      .then(function (links){
-        $scope.data.links = links;
-      })
-      .catch(function (err){
-        console.error('Error while loading initial links from LinksController ', err);
-      });
-    };
-    //console.log($scope.data);
-    initialize();
-
+  var initialize = function () {
+    Links.getAll()
+    .then(function (links){
+      $scope.data.links = links;
+    })
+    .catch(function (err){
+      console.error('Error while loading initial links from LinksController ', err);
+    });
+  };
+  //console.log($scope.data);
+  initialize();
 });
